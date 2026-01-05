747a059826bc427c76b9c98562d7956ab0b34acd
Jamik <dzamikhidirov8@gmail.com>
2026-01-05 22:10:21 +0300

    добавлены основные методы запросов

--- core/network/src/main/java/com/example/network/setvice/WegoExcursionService.kt
+++ core/network/src/main/java/com/example/network/setvice/WegoExcursionService.kt
@@ -2,6 +2,7 @@ package com.example.network.setvice
 
 import com.example.network.state.WeGoApi
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 
@@ -9,7 +10,7 @@ import retrofit2.http.Query
 @WeGoApi
 interface WegoExcursionService {
 
-    @GET("/cities/")
+    @GET("cities")
     suspend fun getListCities(
         //Для плагинации
         @Query("page") page: Int,
@@ -22,7 +23,7 @@ interface WegoExcursionService {
 
 
 
-    @GET(" /attractions/")
+    @GET("attractions")
     //Функция будет возаращать список достопримечательностей
     suspend fun getListattraction(
         //Для плагинации
@@ -35,14 +36,50 @@ interface WegoExcursionService {
 
     )
 
-    @GET(" /search/")
+    @GET("search")
     suspend fun searchList(
         // Строковый поисковый запрос Любые строковые
         @Query("query") query: String,
         //	Код валюты для показа цен у товаров
         //code из GET /currencies/
         @Query("currency") code: String = "RUB"
+    )
 
+
+    @GET("products/popular")
+    suspend fun getPopularProducts(
+        //Для плагинации
+        @Query("page") page: Int,
+        //Код языка товаров (контент, описание)
+        @Query("lang") lang: String = "ru",
+        //Код валют для показа цен у товары
+        @Query("currency") currency: String = "RUB",
+        //Уникальный айди страны к кому хотим сделать поиск
+        //	id из GET /search/ или GET /countries/
+        @Query("country") country: Int,
+        //Уникальный идентификатор города
+        //id из GET /search/ или GET /cities/
+        @Query("city") city: Int,
+        //Уникальный индентификатор достопримечательности
+        //id из GET /search/ или GET /attractions/
+        @Query("attraction") attraction: String,
+        //Тип сортировки
+        //popularity (по умолчанию) или random
+        @Query("order") popularity: String
+    )
+
+
+    @GET("products/{ID}")
+    suspend fun getInfoProducts(
+        @Path("ID") id: Int,
+        @Query("currency") currency: String = "RUB"
+    )
+
+
+    @GET("products/{ID}/reviews")
+    suspend fun getReviews(
+        @Path("ID") id: Int,
+        @Query("page") page: Int
     )
 
 
