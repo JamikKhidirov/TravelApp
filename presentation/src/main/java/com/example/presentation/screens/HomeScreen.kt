cac2428c00c6e45528286ab1b33ce955db59e786
Jamik <dzamikhidirov8@gmail.com>
2026-01-10 02:13:05 +0300

    добавлен шимер загрузки

--- presentation/src/main/java/com/example/presentation/screens/HomeScreen.kt
+++ presentation/src/main/java/com/example/presentation/screens/HomeScreen.kt
@@ -1,16 +1,21 @@
 package com.example.presentation.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -20,7 +25,9 @@ import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScrollModifierNode
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,7 +38,10 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.domain.wegodata.attractiondata.Attraction
 import com.example.domain.wegodata.citiesdata.City
 import com.example.domain.wegodata.productpopular.Tour
+import com.example.presentation.modifiers.shimerEffect
 import com.example.presentation.uicomponents.buttons.MainButton
+import com.example.presentation.uicomponents.dowloads.ShimmerPlaceholder
+import com.example.presentation.uicomponents.dowloads.items.RowItemsShimmerPlaceHolder
 import com.example.presentation.uicomponents.search.SearchCard
 import com.example.presentation.uicomponents.vidjets.PopularTourItem
 import com.example.presentation.uicomponents.vidjets.RowCities
@@ -192,6 +202,12 @@ fun BottomHomeScreen(
 
         //В ров список ближащих или популярных мест
         item {
+            if (listCity.isEmpty()) {
+                RowItemsShimmerPlaceHolder(
+                    modifier = Modifier
+                )
+            }
+
             Column {
                 RowCities<City>(
                     modifier = Modifier,
@@ -211,6 +227,8 @@ fun BottomHomeScreen(
                     )
                 }
             }
+
+
         }
 
         //Популярные места
@@ -224,6 +242,10 @@ fun BottomHomeScreen(
                         fontWeight = FontWeight.Bold,
                         modifier = Modifier.padding(top = 25.dp, start = 15.dp)
                     )
+                }
+
+                if (listAttraction.isEmpty()){
+                    RowItemsShimmerPlaceHolder()
                 }
 
                 RowCities<Attraction>(
