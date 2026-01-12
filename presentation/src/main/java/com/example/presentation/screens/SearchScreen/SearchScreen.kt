40e16d5d9222721af27051ed3499609f47a92480
Jamik <dzamikhidirov8@gmail.com>
2026-01-12 21:48:41 +0300

    добавлен виджет поиска

--- presentation/src/main/java/com/example/presentation/screens/SearchScreen/SearchScreen.kt
+++ presentation/src/main/java/com/example/presentation/screens/SearchScreen/SearchScreen.kt
@@ -1,12 +1,17 @@
 package com.example.presentation.screens.SearchScreen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.presentation.screens.SearchScreen.uicomponents.SearchBar
 
 
 @Composable
@@ -16,6 +21,7 @@ fun SearchScreen(){
 
     Scaffold(
         modifier = Modifier.fillMaxSize()
+            .background(MaterialTheme.colorScheme.primary)
     ) { paddingValues ->
         SearchBottomScreen(
             paddingValues = paddingValues
@@ -28,5 +34,9 @@ fun SearchScreen(){
 fun SearchBottomScreen(
     paddingValues: PaddingValues
 ){
+    SearchBar(
+        modifier = Modifier.fillMaxWidth()
+            .padding(horizontal = 15.dp)
+    ) {  }
 
 }
---
+++ presentation/src/main/java/com/example/presentation/screens/SearchScreen/uicomponents/SearchBar.kt
@@ -0,0 +1,77 @@
+package com.example.presentation.screens.SearchScreen.uicomponents
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlin.math.sin
+
+
+@Composable
+@Preview(showBackground = true)
+fun SearchBar(
+    modifier: Modifier = Modifier,
+    onTextValue: (String) -> Unit = {}
+){
+
+    var textValue by remember { mutableStateOf("") }
+
+    TextField(
+        value = textValue,
+        modifier = modifier,
+        onValueChange = onTextValue,
+        shape = RoundedCornerShape(15.dp),
+        singleLine = true,
+        colors = TextFieldDefaults.colors(
+            disabledIndicatorColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledContainerColor = MaterialTheme.colorScheme.background
+        ),
+
+        placeholder = {
+            Text(
+                text = "Город, экскурсия, билет, доставка",
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1,
+                modifier = Modifier.padding(start = 10.dp),
+                fontSize = 19.sp
+            )
+        },
+
+        trailingIcon = {
+            if (textValue.isNotEmpty()){
+                IconButton(
+                    modifier = Modifier.size(32.dp),
+                    onClick = {
+                        textValue = ""
+                    }
+                ) {
+                    Icon(
+                        Icons.Default.Clear,
+                        contentDescription = "Иконка для очистки"
+                    )
+                }
+            }
+        }
+    )
+}
