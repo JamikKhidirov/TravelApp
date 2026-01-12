539290315495ebce314b214de787e5695ac6d6ad
Jamik <dzamikhidirov8@gmail.com>
2026-01-12 22:49:43 +0300

    добавлен макет экрана

--- presentation/src/main/java/com/example/presentation/screens/SearchScreen/SearchScreen.kt
+++ presentation/src/main/java/com/example/presentation/screens/SearchScreen/SearchScreen.kt
@@ -6,12 +6,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyItemScope
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigationevent.NavigationEventHistory
 import com.example.presentation.screens.SearchScreen.uicomponents.SearchBar
 
 
@@ -23,10 +30,21 @@ fun SearchScreen(){
     Scaffold(
         modifier = Modifier.fillMaxSize()
             .background(MaterialTheme.colorScheme.background)
+        ,
+        topBar = {
+            SearchBar(
+                modifier = Modifier.fillMaxWidth()
+                    .padding(horizontal = 15.dp),
+                onTextValue = {
+
+                }
+            )
+        }
     ) { paddingValues ->
         SearchBottomScreen(
             paddingValues = paddingValues,
-            onBarSeacrch = { searchText ->
+            history = listOf(),
+            onHistory = {
 
             }
         )
@@ -37,7 +55,10 @@ fun SearchScreen(){
 @Composable
 fun SearchBottomScreen(
     paddingValues: PaddingValues,
-    onBarSeacrch: (String) -> Unit
+    history: List<String>,
+    popularList: List<String>? = null,
+    onHistory: (String) -> Unit
+
 ){
     LazyColumn(
         modifier = Modifier.fillMaxSize()
@@ -45,14 +66,27 @@ fun SearchBottomScreen(
     ) {
 
         item {
-            SearchBar(
-                modifier = Modifier.fillMaxWidth()
-                    .padding(horizontal = 15.dp),
-                onTextValue = onBarSeacrch
+            Text(
+                text = "Популярные места",
+                modifier = Modifier.padding(
+                    horizontal = 15.dp,
+                    vertical = 10.dp
+                ),
+                fontSize = 22.sp,
+                color = Color.Black,
+                fontWeight = FontWeight.Bold
             )
+
+        }
+
+        if (popularList?.isNotEmpty() == true){
+            item(popularList){
+                //Виджет истории поиска
+            }
         }
 
 
-    }
 
+
+    }
 }
