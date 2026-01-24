5e0d6029ba94b5651f467affd621ee7de89b7117
Jamik <dzamikhidirov8@gmail.com>
2026-01-24 18:47:48 +0300

    Исправляем Активность

--- app/src/main/java/com/example/travelapp/MainActivity.kt
+++ app/src/main/java/com/example/travelapp/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.travelapp
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.navigation.compose.rememberNavController
 import com.example.navigation.NavHostApp
 import com.example.travelapp.ui.theme.TravelAppTheme
@@ -11,9 +15,20 @@ import dagger.hilt.android.AndroidEntryPoint
 
 
 
-
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+
+    private val locationPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            //Разрешение выдано можно вытаскивать геолоквцию пользователя
+        } else {
+            // Показать сообщение: "Нужно разрешение для работы с геолокацией"
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -25,7 +40,17 @@ class MainActivity : ComponentActivity() {
                 )
             }
         }
+    }
 
+
+    //Функция проверяет есть ли разрешение на приблизительную или точную геолокацию
+    private fun hasLocationPermission(): Boolean {
+        return listOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ).any { permission ->
+            ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
+        }
     }
 }
 
