abd6c304139a6ecc65320dac5d1e8ad17650d99c
Jamik <dzamikhidirov8@gmail.com>
2026-01-24 18:28:09 +0300

    Добавлены UseCase

---
+++ core/location/src/main/java/com/example/location/domain/usecases/GetCurrentLocationUseCase.kt
@@ -0,0 +1,9 @@
+package com.example.location.domain.usecases
+
+import javax.inject.Inject
+import javax.inject.Singleton
+
+
+@Singleton
+class GetCurrentLocationUseCase @Inject constructor() {
+}
---
+++ core/location/src/main/java/com/example/location/domain/usecases/GetLocationUpdates.kt
@@ -0,0 +1,11 @@
+package com.example.location.domain.usecases
+
+import javax.inject.Inject
+import javax.inject.Singleton
+
+
+@Singleton
+class GetLocationUpdates @Inject constructor(
+
+) {
+}
