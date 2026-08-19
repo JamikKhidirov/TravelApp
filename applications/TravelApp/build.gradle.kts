cb659930a95ff9808d69df9f08db86d145020093
Jamik <dzamikhidirov8@gmail.com>
2026-08-19 23:07:14 +0300

    Временно отючен R8

--- applications/TravelApp/build.gradle.kts
+++ applications/TravelApp/build.gradle.kts
@@ -42,7 +42,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            isShrinkResources= true
+            isShrinkResources= false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
